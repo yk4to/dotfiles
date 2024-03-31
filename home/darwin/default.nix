8b991db99920440a16a76a4c61e2a8b1be23196b
fus1ondev <luke_s1220@yahoo.co.jp>
2024-03-31 20:51:18 +0900

    feat(nix): add darwin apps settings

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -8,7 +8,4 @@
     homeDirectory = "/Users/${username}";
     stateVersion = "23.11";
   };
-
-  # to set Nix-related paths to zsh
-  programs.zsh.enable = true;
 }
---
+++ modules/darwin/apps.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    fish
+  ];
+
+  programs.zsh.enable = true;
+  environment.shells = [
+    pkgs.zsh
+    pkgs.fish
+  ];
+
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "zap";
+    };
+
+    masApps = {
+      # Xcode = 497799835;
+    };
+
+    taps = [
+      # "homebrew/cask-fonts"
+    ];
+
+    brews = [
+
+    ];
+
+    casks = [
+
+    ];
+  }
+}
