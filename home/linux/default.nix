9040921feec31c06e7c4dbf0611cee46994a8564
yk4to <64204135+yk4to@users.noreply.github.com>
2024-04-01 13:09:57 +0900

    feat(linux): change user dir names to English

--- home/linux/default.nix
+++ home/linux/default.nix
@@ -11,4 +11,16 @@
     homeDirectory = "/home/${username}";
     stateVersion = "23.11";
   };
+
+  # change user dir names to English
+  environment.etc."xdg/user-dirs.defaults".text = ''
+    DESKTOP=Desktop
+    DOCUMENTS=Documents
+    DOWNLOAD=Downloads
+    MUSIC=Music
+    PICTURES=Pictures
+    PUBLICSHARE=Public
+    TEMPLATES=Templates
+    VIDEOS=Videos
+  '';
 }
