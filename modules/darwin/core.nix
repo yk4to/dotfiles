99196d6714bcc40c5d9757d5f866d45596f51707
yk4to <64204135+yk4to@users.noreply.github.com>
2024-04-01 15:04:10 +0900

    feat(nix-darwin): setup gc

--- modules/darwin/core.nix
+++ modules/darwin/core.nix
@@ -7,5 +7,10 @@
 
   # ref: https://github.com/NixOS/nix/issues/7273
   nix.settings.auto-optimise-store = false;
-  nix.gc.automatic = false;
+  
+  nix.gc = {
+    automatic = true;
+    interval = { Weekday = 0; Hour = 0; Minute = 0; };
+    options = "--delete-older-than 30d";
+  };
 }
