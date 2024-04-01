9af3ce67a912c76ff6a8c0b31439b70d30d9694e
yk4to <64204135+yk4to@users.noreply.github.com>
2024-04-02 00:07:27 +0900

    fix(systemd-boot): change wrong config

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -23,11 +23,14 @@
   networking.hostName = "thinkpad";
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      extraInstallCommands = "echo 'default @saved' > /boot/loader/loader.conf";
+    };
 
-  # save default item
-  boot.loader.grub.default = "saved";
+    efi.canTouchEfiVariables = true;
+  }
 
   # Enable networking
   networking.networkmanager.enable = true;
