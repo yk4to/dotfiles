67131e9a3f824b98fd6b9877f6ea37b4302f901b
yk4to <64204135+yk4to@users.noreply.github.com>
2024-04-01 12:52:50 +0900

    feat(linux): enable wezterm

--- home/linux/apps.nix
+++ home/linux/apps.nix
@@ -7,5 +7,29 @@
 
   programs = {
     google-chrome.enable = true;
+
+    wezterm = {
+      enable = true;
+      extraConfig = ''
+        local wezterm = require("wezterm")
+        local config = {
+          font = wezterm.font("UDEV Gothic 35NFLG"),
+          font_size = 13.0,
+          color_scheme = "OneHalfDark",
+          -- use_fancy_tab_bar = false,
+          -- freetype_load_target = "Light",
+          -- window_background_opacity = 0.95,
+          hide_tab_bar_if_only_one_tab = true,
+          -- window_decorations = "RESIZE",
+          window_padding = {
+            left = 14,
+            right = 14,
+            top = 14, -- 54
+            bottom = 14,
+          },
+        }
+        return config
+      '';
+    }
   };
 }
