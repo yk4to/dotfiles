3d4396d7fafd7c5cb57d238e64dd8c5ec1810302
yk4to <64204135+yk4to@users.noreply.github.com>
2024-04-01 11:44:30 +0900

    feat(nix): load zoxide in fish shell

--- home/base/fish.nix
+++ home/base/fish.nix
@@ -17,6 +17,22 @@
       if test -n "$GHOSTTY_RESOURCES_DIR"
         builtin source "''$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
       end
+
+      # cache
+      # ref: https://zenn.dev/ryoppippi/articles/de6c931cc1028f
+      set -l CONFIG_CACHE $HOME/.cache/fish/config.fish
+      if test "$HOME/.config/fish/config.fish" -nt "$CONFIG_CACHE"
+          mkdir -p $HOME/.cache/fish
+          echo "" >$CONFIG_CACHE
+
+          # tools
+          type -q zoxide && zoxide init fish >>$CONFIG_CACHE
+
+          set_color brmagenta --bold --underline
+          echo "cache updated"
+          set_color normal
+      end
+      source $CONFIG_CACHE
     '';
   };
 }
