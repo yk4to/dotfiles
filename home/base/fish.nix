4b7542fd7b0fb86bc6367ee3a070812d3d86264a
yk4to <64204135+yk4to@users.noreply.github.com>
2024-04-01 09:44:54 +0900

    feat(ghostty): enable fish integration

--- home/base/fish.nix
+++ home/base/fish.nix
@@ -9,8 +9,14 @@
     interactiveShellInit = ''
       # disable greeting
       set -g fish_greeting
+
       # starship
       source ~/dotfiles/fish/starship_async_transient_prompt.fish
+
+      # ghostty shell integration
+      if test -n "$GHOSTTY_RESOURCES_DIR"
+        builtin source "''$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
+      end
     '';
   };
 }
