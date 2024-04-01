f50d03b8e26b599c57a518b2c4452f1a9d31325a
yk4to <64204135+yk4to@users.noreply.github.com>
2024-04-01 13:24:46 +0900

    refactor(nixos): change deprecated key names

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -15,8 +15,8 @@
   services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 }
