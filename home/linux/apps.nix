e0e8e8dce2e180eacbb96be48fa4468371bfe6a4
yk4to <64204135+yk4to@users.noreply.github.com>
2024-05-15 15:48:50 +0900

    feat(home): remove wezterm

--- home/linux/apps.nix
+++ home/linux/apps.nix
@@ -5,29 +5,5 @@
     vscode
   ];
 
-  programs = {
-    wezterm = {
-      enable = true;
-      extraConfig = ''
-        local wezterm = require("wezterm")
-        local config = {
-          font = wezterm.font("UDEV Gothic 35NFLG"),
-          font_size = 10.0,
-          color_scheme = "OneHalfDark",
-          -- use_fancy_tab_bar = false,
-          -- freetype_load_target = "Light",
-          -- window_background_opacity = 0.95,
-          hide_tab_bar_if_only_one_tab = false,
-          hide_mouse_cursor_when_typing = false,
-          window_padding = {
-            left = 14,
-            right = 14,
-            top = 14, -- 54
-            bottom = 14,
-          },
-        }
-        return config
-      '';
-    };
-  };
+  # programs = {};
 }
