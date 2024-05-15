8ee47b532119f53a3694bed9a6c465f980d0e01b
yk4to <64204135+yk4to@users.noreply.github.com>
2024-05-16 00:01:14 +0900

    feat(hypr): enable fractional scaling

--- home/linux/hyprland.nix
+++ home/linux/hyprland.nix
@@ -6,9 +6,11 @@
       "$mod" = "SUPER";
       bind = [
         "$mod, F, exec, firefox"
-        "$mod, C, exec, code"
+        "$mod, C, exec, code --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto"
         "$mod, g, exec, ghostty --window-decoration=false"
       ];
+
+      monitor = "eDP-1, 2880x1800@90, 0x0, 1.8";
     };
   };
 }
