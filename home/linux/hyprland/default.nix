6146cdd942e8c736aa7fd00bf185438a4598f263
yk4to <64204135+yk4to@users.noreply.github.com>
2024-05-17 16:12:53 +0900

    feat(hypr): add config related to look and feel

--- home/linux/hyprland/default.nix
+++ home/linux/hyprland/default.nix
@@ -19,6 +19,46 @@
         "eDP-1, 2880x1800@90, 0x0, 1.8"
         "HDMI-A-1, 3840x2160@60, 0x0, 1.5"
       ];
+
+      # --- LOOK AND FEEL ---
+      general = {
+        gaps_in = 5;
+        gaps_out = 20;
+
+        border_size = 2;
+        "col.active_border" = "rgba(56b6c2ee) rgba(61afefee) 45deg";
+        "col.inactive_border" = "rgba(5c6370aa)";
+
+        # Set to true enable resizing windows by clicking and dragging on borders and gaps
+        resize_on_border = true;
+
+        # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+        allow_tearing = false;
+
+        layout = "dwindle";
+      };
+
+      decoration = {
+        rounding = 10;
+
+        # Change transparency of focused and unfocused windows
+        active_opacity = 1.0;
+        inactive_opacity = 1.0;
+
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+
+        # https://wiki.hyprland.org/Configuring/Variables/#blur
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+          
+          vibrancy = 0.1696;
+        };
+      };
     };
   };
 }
