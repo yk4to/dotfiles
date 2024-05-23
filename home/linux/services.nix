9e2091e48b956bff797ab6f869dcaba6e5d6bf54
yk4to <64204135+yk4to@users.noreply.github.com>
2024-05-23 23:00:35 +0900

    fix(nix): correct wrong keys

--- home/linux/services.nix
+++ home/linux/services.nix
@@ -1,9 +1,13 @@
 { pkgs, ... }: {
   # launch 1Password at login
   systemd.user.services._1password = {
-    after = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    wantedBy = [ "graphical-session.target" ];
+    Unit = {
+      After = [ "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
     Service = {
       SyslogIdentifier = "1password";
       ExecStart = "${pkgs._1password-gui}/bin/1password --silent";
