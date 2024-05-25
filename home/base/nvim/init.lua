637583a640edf89136d8c20c6d74b71d9a61aafa
yk4to <64204135+yk4to@users.noreply.github.com>
2024-05-25 11:35:14 +0900

    feat(nvim): add vim config

--- home/base/nvim/init.lua
+++ home/base/nvim/init.lua
@@ -1,3 +1,11 @@
+vim.opt.number = true
+
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+
 local lazypath = "@lazy_nvim@"
 vim.opt.rtp:prepend(lazypath)
 
