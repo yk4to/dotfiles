135b59652202a29925727dd02cfb6e7af713e157
yk4to <64204135+yk4to@users.noreply.github.com>
2024-05-25 10:38:36 +0900

    feat(nvim): show lsp on lualine

--- home/base/nvim/lua/plugins/ui.lua
+++ home/base/nvim/lua/plugins/ui.lua
@@ -1,11 +1,37 @@
+-- ref: https://qiita.com/uhooi/items/99aeff822d4870a8e269
+local lsp_names = function()
+  local clients = {}
+  for _, client in ipairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+    --[[ if client.name == "null-ls" then
+      local sources = {}
+      for _, source in ipairs(require("null-ls.sources").get_available(vim.bo.filetype)) do
+        table.insert(sources, source.name)
+      end
+      table.insert(clients, "null-ls(" .. table.concat(sources, ", ") .. ")")
+    else --]]
+      table.insert(clients, client.name)
+    -- end
+  end
+  if next(clients) then
+    return " " .. table.concat(clients, ", ")
+  else
+    return " No LSP"
+  end
+end
+
 return {
   {
     name = "lualine.nvim",
     dir = "@lualine_nvim@",
     dependencies = { { name = "nvim-web-devicons", dir = "@nvim_web_devicons@" } },
     opts = {
-      theme = "onedark",
-    }
+      options = {
+        theme = "onedark",
+      },
+      sections = {
+        lualine_x = { 'encoding', 'fileformat', 'filetype', lsp_names },
+      },
+    },
   },
   {
     name = "noice.nvim",
