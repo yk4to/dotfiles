25ab4e443d51b419d81136cab09e580e2aea4dff
yk4to <64204135+yk4to@users.noreply.github.com>
2024-05-25 11:35:35 +0900

    feat(nvim): disable arrow keys and backspace

--- home/base/nvim/init.lua
+++ home/base/nvim/init.lua
@@ -6,6 +6,19 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 
+-- disable allow keys
+vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Right>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<BS>', '<Nop>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<BS>', '<Nop>', { noremap = true, silent = true })
+
 local lazypath = "@lazy_nvim@"
 vim.opt.rtp:prepend(lazypath)
 
