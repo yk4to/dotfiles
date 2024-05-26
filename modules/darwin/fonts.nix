5295a20476ed5f29706cb17a509f16a82b048803
yk4to <64204135+yk4to@users.noreply.github.com>
2024-05-26 18:32:40 +0900

    feat(ghostty): set fallback font

--- modules/darwin/fonts.nix
+++ modules/darwin/fonts.nix
@@ -10,7 +10,7 @@
       udev-gothic
       udev-gothic-nf
 
-      # (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     ];
   };
 }
