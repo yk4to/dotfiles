8c749c0799bc2236e996b32eb92db11201c5c0c8
yk4to <64204135+yk4to@users.noreply.github.com>
2024-05-27 18:10:17 +0900

    refactor(darwin): move icons to iCloud

--- modules/darwin/icons.nix
+++ modules/darwin/icons.nix
@@ -1,51 +1,54 @@
-{
+let 
+  icloudPath = "/Users/yuta/Library/Mobile Documents/com~apple~CloudDocs";
+  iconsPath = "${icloudPath}/icons";
+in {
   # ref: https://github.com/ryanccn/nix-darwin-custom-icons
   environment.customIcons = {
     enable = true;
     icons = [
       {
         path = "/Applications/AltTab.app";
-        icon = ../../icons/alttab.icns;
+        icon = "${iconsPath}/alttab.icns";
       }
       {
         path = "/Applications/KeyboardCleanTool.app/";
-        icon = ../../icons/keyboardcleantool.icns;
+        icon = "${iconsPath}/keyboardcleantool.icns";
       }
       {
         path = "/Applications/Blackmagic RAW/Blackmagic RAW Player.app/";
-        icon = ../../icons/rawplayer.icns;
+        icon = "${iconsPath}/rawplayer.icns";
       }
       {
         path = "/Applications/Blackmagic RAW/Blackmagic RAW Speed Test.app/";
-        icon = ../../icons/rawspeedtest.icns;
+        icon = "${iconsPath}/rawspeedtest.icns";
       }
       {
         path = "/Applications/Google Docs.app/";
-        icon = ../../icons/googledocs.icns;
+        icon = "${iconsPath}/googledocs.icns";
       }
       {
         path = "/Applications/Google Sheets.app/";
-        icon = ../../icons/googlesheets.icns;
+        icon = "${iconsPath}/googlesheets.icns";
       }
       {
         path = "/Applications/Google Slides.app/";
-        icon = ../../icons/googleslides.icns;
+        icon = "${iconsPath}/googleslides.icns";
       }
       {
         path = "/Applications/Box.app/";
-        icon = ../../icons/box.icns;
+        icon = "${iconsPath}/box.icns";
       }
       {
         path = "/Applications/F5 VPN.app/";
-        icon = ../../icons/f5vpn.icns;
+        icon = "${iconsPath}/f5vpn.icns";
       }
       {
         path = "/Applications/Raspberry Pi Imager.app/";
-        icon = ../../icons/raspi.icns;
+        icon = "${iconsPath}/raspi.icns";
       }
       {
         path = "/Applications/MATLAB_R2024a.app/";
-        icon = ../../icons/matlab.icns;
+        icon = "${iconsPath}/matlab.icns";
       }
     ];
   };
