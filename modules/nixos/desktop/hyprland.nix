88f08f9506a63eda676576e3163e6cfbadf56c71
yk4to <64204135+yk4to@users.noreply.github.com>
2024-06-02 16:20:57 +0900

    feat(nix): add cache config

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -3,12 +3,6 @@
   pkgs,
   ...
 }: {
-  # enable Cachix cache
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
-
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
