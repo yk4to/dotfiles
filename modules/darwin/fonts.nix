14a8a705ac067794c30134ce28876c1a66a0a143
yk4to <64204135+yk4to@users.noreply.github.com>
2024-06-21 18:03:15 +0900

    fix(darwin): follow nix-darwin changes about fonts

--- modules/darwin/fonts.nix
+++ modules/darwin/fonts.nix
@@ -1,12 +1,6 @@
 {pkgs, ...}: {
   fonts = {
-    # will be removed after this PR is merged:
-    #   https://github.com/LnL7/nix-darwin/pull/754
-    fontDir.enable = true;
-
-    # will change to `fonts.packages` after this PR is merged:
-    #   https://github.com/LnL7/nix-darwin/pull/754
-    fonts = with pkgs; [
+    packages = with pkgs; [
       udev-gothic
       udev-gothic-nf
 
