a733fb1d54cf9e3bbd7fc0206994ca6618ae3e70
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-08-18 00:04:17 +0900

    Revert "refactor(raspi): avoid licensing issue in a hacky way"
    This reverts commit d4aefa155fb92ed5bdb5577d8989f1600a13bab0.

--- flake.nix
+++ flake.nix
@@ -34,11 +34,7 @@
 
     darwin-custom-icons.url = "github:ryanccn/nix-darwin-custom-icons";
 
-    ghostty = {
-      url = "git+ssh://git@github.com/ghostty-org/ghostty";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
-      inputs.nixpkgs-unstable.follows = "nixpkgs";
-    };
+    ghostty.url = "git+ssh://git@github.com/ghostty-org/ghostty";
     ghostty-module.url = "github:clo4/ghostty-hm-module";
 
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
