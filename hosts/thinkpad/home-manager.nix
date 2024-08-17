7dd7d977f17f72b3805fa83199e18fa4deccdbff
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-08-17 16:05:12 +0900

    refactor(home): add gui dir

--- hosts/thinkpad/home-manager.nix
+++ hosts/thinkpad/home-manager.nix
@@ -4,7 +4,7 @@
     ../../home/linux/optional/gui
     ../../home/linux/optional/gnome
     ../../home/linux/optional/hyprland
-    ../../home/base/optional/ghostty.nix
+    ../../home/base/optional/gui
     # ../../home/base/optional/latex.nix
     # ../../home/base/optional/platformio.nix
     ../../home/base/optional/vscode.nix
