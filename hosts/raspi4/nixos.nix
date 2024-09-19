dcc0e0817f8424a0110b990c0e45cc8d22e9f9c2
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-09-19 18:13:32 +0900

    feat(raspi): enable auto login

--- hosts/raspi4/nixos.nix
+++ hosts/raspi4/nixos.nix
@@ -60,6 +60,13 @@ in {
     };
   };
 
+  # Enable automatic login
+  # ref: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = vars.username;
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
     initrd.availableKernelModules = ["xhci_pci" "usbhid" "usb_storage"];
