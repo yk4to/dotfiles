0df4b70400b1c67ad93b0398dadda32c6cee9f9c
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-10-28 00:38:49 +0900

    Revert "feat(raspi): set up dns experimentally"
    This reverts commit c09c96f60c104ddc7a58925387c380fa52592470.

--- hosts/raspi4/nixos.nix
+++ hosts/raspi4/nixos.nix
@@ -26,16 +26,6 @@
 
   networking.hostName = "raspi4";
 
-  networking.nameservers = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
-
-  services.resolved = {
-    enable = true;
-    dnssec = "true";
-    domains = ["~."];
-    fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
-    dnsovertls = "true";
-  };
-
   # Enable ssh
   services.openssh.enable = true;
 
