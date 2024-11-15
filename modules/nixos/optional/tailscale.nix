0839029efdd3fbe73313ed0d39b26fd7eabbd173
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-11-15 23:21:00 +0900

    feat(nixos): add tailscale firewall config

--- modules/nixos/optional/tailscale.nix
+++ modules/nixos/optional/tailscale.nix
@@ -8,6 +8,19 @@
 
   services.tailscale.enable = true;
 
+  networking.firewall = {
+    enable = true;
+
+    # always allow traffic from your Tailscale network
+    trustedInterfaces = ["tailscale0"];
+
+    # allow the Tailscale UDP port through the firewall
+    allowedUDPPorts = [config.services.tailscale.port];
+
+    # let you SSH in over the public internet
+    networking.firewall.allowedTCPPorts = [22];
+  };
+
   systemd.services.tailscale-autoconnect = {
     description = "Automatic connection to Tailscale";
 
