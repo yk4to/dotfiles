d9b76fc1cbe87dfc64e3805636fc7446150d94c8
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-11-22 18:00:06 +0900

    Revert "fix(nixos): disable fcitx5-mozc while it is broken"
    This reverts commit 766b6951ecc662bead2286fb387c2a3d39e90122.

--- modules/nixos/optional/gui/ime.nix
+++ modules/nixos/optional/gui/ime.nix
@@ -2,7 +2,6 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    # TODO: uncomment this when #355852 is solved
-    # fcitx5.addons = [pkgs.fcitx5-mozc];
+    fcitx5.addons = [pkgs.fcitx5-mozc];
   };
 }
