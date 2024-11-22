ef5940a8690e77a2dc46c2c4ea09f52cc384ea66
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-11-22 20:22:37 +0900

    feat(gnome): replace paperwm with pop shell

--- home/linux/optional/gnome/extensions.nix
+++ home/linux/optional/gnome/extensions.nix
@@ -1,16 +1,20 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    gnomeExtensions.paperwm
-    gnomeExtensions.dash-to-dock
-    gnomeExtensions.kimpanel
+  home.packages = with pkgs.gnomeExtensions; [
+    pop-shell # tiling window manager
+    dash-to-dock # dock used in Ubuntu
+    kimpanel # show ime status
   ];
 
   dconf.settings = {
     "org/gnome/shell".enabled-extensions = [
-      "paperwm@paperwm.github.com"
+      "pop-shell@system76.com"
       "dash-to-dock@micxgx.gmail.com"
       "kimpanel@kde.org"
     ];
+
+    "org/gnome/shell/extensions/pop-shell" = {
+      tile-by-default = true;
+    };
 
     "org/gnome/shell/extensions/dash-to-dock" = {
       dock-position = "LEFT";
