c84550c6db5e5a87b6e57c96d0b88cd30c6cd7c2
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-11-22 21:46:45 +0900

    feat(gnome): add extensions

--- home/linux/optional/gnome/extensions.nix
+++ home/linux/optional/gnome/extensions.nix
@@ -1,8 +1,14 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   home.packages = with pkgs.gnomeExtensions; [
     pop-shell # tiling window manager
     dash-to-dock # dock used in Ubuntu
-    kimpanel # show ime status
+    kimpanel # show ime status on menu bar
+    appindicator # show indicator icons on menu bar
+    logo-menu
   ];
 
   dconf.settings = {
@@ -10,6 +16,8 @@
       "pop-shell@system76.com"
       "dash-to-dock@micxgx.gmail.com"
       "kimpanel@kde.org"
+      "appindicatorsupport@rgcjonas.gmail.com"
+      "logomenu@aryan_k"
     ];
 
     "org/gnome/shell/extensions/pop-shell" = {
@@ -25,6 +33,14 @@
 
       apply-custom-theme = true;
       custom-theme-shrink = true;
+    };
+
+    "org/gnome/shell/extensions/Logo-menu" = {
+      menu-button-icon-image = 23;
+      menu-button-icon-size = 23;
+      menu-button-icon-click-type = 3;
+      menu-button-terminal = "ghostty";
+      hide-softwarecentre = true;
     };
   };
 }
