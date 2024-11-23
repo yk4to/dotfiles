aa03c3d3467398af0212d95eef89ba61b99d9f2b
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-11-23 10:02:43 +0900

    refactor(nix): move config from `flake.nix`

--- flake.nix
+++ flake.nix
@@ -1,16 +1,4 @@
 {
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org/"
-      "https://ghostty.cachix.org/"
-    ];
-
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
--- modules/base/default.nix
+++ modules/base/default.nix
@@ -9,7 +9,19 @@
   # enable experimental features
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
-    trusted-users = [vars.username];
+    trusted-users = ["root" vars.username];
+    accept-flake-config = true;
+
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://ghostty.cachix.org/"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
+    ];
   };
 
   nixpkgs.config.allowUnfree = true;
