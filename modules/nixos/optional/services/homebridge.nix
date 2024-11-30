666a694407ac7094c6a50fb9b36625ec334893d3
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-11-30 22:39:17 +0900

    fix(nix): use oci-containers instead of arion

--- modules/nixos/optional/services/homebridge.nix
+++ modules/nixos/optional/services/homebridge.nix
@@ -1,35 +1,14 @@
 {
-  virtualisation.arion.projects.homebridge.settings = {
-    services.homebridge.service = {
-      image = "homebridge/homebridge:latest";
-      container_name = "homebridge";
-      volumes = [
-        {
-          type = "volume";
-          source = "homebridge";
-          target = "/homebridge";
-        }
-      ];
-      restart = "always";
-      nerwork_mode = "host";
-      # logging = {
-      #   driver = "json-file";
-      #   options = {
-      #     max-size = "10m";
-      #     max-file = "1";
-      #   };
-      # };
-      healthcheck = {
-        test = ["CMD-SHELL" "curl --fail http://localhost:8581 || exit 1"];
-        interval = "60s";
-        retries = 5;
-        start_period = "300s";
-        timeout = "2s";
-      };
+  virtualisation.oci-containers.containers.homebridge = {
+    image = "docker.io/homebridge/homebridge:latest";
+    volumes = ["/var/lib/homebridge:/homebridge"];
+    environment = {
+      "TZ" = "Asia/Tokyo";
+      "HOMEBRIDGE_CONFIG_UI" = "1";
+      "HOMEBRIDGE_CONFIG_UI_PORT" = "8581";
     };
-
-    docker-compose.volumes = {
-      homebridge = {};
-    };
+    extraOptions = ["--network=host"];
   };
+
+  networking.firewall.allowedTCPPorts = [8581];
 }
