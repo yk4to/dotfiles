47d47e603526295f7cb1ce4e2547f688fd31cafc
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-11-30 15:58:01 +0900

    fix(nix): add missing `enable`

--- home/base/optional/ghostty.nix
+++ home/base/optional/ghostty.nix
@@ -13,6 +13,8 @@ in {
 
   config = mkIf cfg.enable {
     programs.ghostty = {
+      enable = true;
+
       shellIntegration.enable = false;
       shellIntegration.enableFishIntegration = true;
 
