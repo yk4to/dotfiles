cd7c61890d54b93f4f4aaa0f7dee8e1540387e9a
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-11-30 18:00:20 +0900

    fix(arion): remove unsupported config

--- modules/nixos/optional/services/homebridge.nix
+++ modules/nixos/optional/services/homebridge.nix
@@ -12,13 +12,13 @@
       ];
       restart = "always";
       nerwork_mode = "host";
-      logging = {
-        driver = "json-file";
-        options = {
-          max-size = "10m";
-          max-file = "1";
-        };
-      };
+      # logging = {
+      #   driver = "json-file";
+      #   options = {
+      #     max-size = "10m";
+      #     max-file = "1";
+      #   };
+      # };
       healthcheck = {
         test = ["CMD-SHELL" "curl --fail http://localhost:8581 || exit 1"];
         interval = "60s";
