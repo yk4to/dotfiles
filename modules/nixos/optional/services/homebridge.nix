ac68acd8d7c379436a6a0a74fd05ec1350a53c74
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-11-30 17:52:38 +0900

    feat(services): add homebridge

---
+++ modules/nixos/optional/services/homebridge.nix
@@ -0,0 +1,35 @@
+{
+  virtualisation.arion.projects.homebridge.settings = {
+    services.homebridge.service = {
+      image = "homebridge/homebridge:latest";
+      container_name = "homebridge";
+      volumes = [
+        {
+          type = "volume";
+          source = "homebridge";
+          target = "/homebridge";
+        }
+      ];
+      restart = "always";
+      nerwork_mode = "host";
+      logging = {
+        driver = "json-file";
+        options = {
+          max-size = "10m";
+          max-file = "1";
+        };
+      };
+      healthcheck = {
+        test = ["CMD-SHELL" "curl --fail http://localhost:8581 || exit 1"];
+        interval = "60s";
+        retries = 5;
+        start_period = "300s";
+        timeout = "2s";
+      };
+    };
+
+    docker-compose.volumes = {
+      homebridge = {};
+    };
+  };
+}
