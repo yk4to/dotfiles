b2349ec9548468194f68a8950615d87ce5714cc8
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2024-12-11 15:23:15 +0900

    chore(firefox): set bookmarks

--- home/linux/optional/gui-apps/firefox.nix
+++ home/linux/optional/gui-apps/firefox.nix
@@ -14,6 +14,7 @@
           "browser.newtabpage.activity-stream.showSponsored" = false;
           "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
         };
+
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
           ublacklist
@@ -22,6 +23,29 @@
           translate-web-pages
           control-panel-for-twitter
           wappalyzer
+        ];
+
+        bookmarks = [
+          {
+            name = "YouTube";
+            url = "https://www.youtube.com/";
+          }
+          {
+            name = "GitHub";
+            url = "https://github.com/";
+          }
+          {
+            name = "NixOS Search - Packages";
+            url = "https://search.nixos.org/packages?channel=unstable";
+          }
+          {
+            name = "Home Manager - Option Search";
+            url = "https://home-manager-options.extranix.com/";
+          }
+          {
+            name = "Darwin Configuration Options";
+            url = "https://daiderd.com/nix-darwin/manual/index.html";
+          }
         ];
       };
     };
