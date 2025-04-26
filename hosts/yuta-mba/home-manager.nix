e5c11baca25e550e899f54a9d182fe98c52da940
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-04-26 16:19:17 +0900

    feat(darwin): add inkdrop module

--- hosts/yuta-mba/home-manager.nix
+++ hosts/yuta-mba/home-manager.nix
@@ -1,10 +1,15 @@
 {
   home.stateVersion = "23.11";
 
-  optionalModules.base = {
-    ghostty.enable = true;
-    latex.enable = true;
-    platformio.enable = true;
-    vscode.enable = true;
+  optionalModules = {
+    base = {
+      ghostty.enable = true;
+      latex.enable = true;
+      platformio.enable = true;
+      vscode.enable = true;
+    };
+    darwin = {
+      inkdrop.enable = true;
+    };
   };
 }
