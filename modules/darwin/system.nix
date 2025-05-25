e2caf084825b805a8e34b86205920ae840b4ff32
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-05-25 13:26:29 +0900

    fix(darwin): follow nix-darwin changes

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,4 +1,4 @@
-{
+{vars, ...}: {
   # use TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
 
@@ -10,12 +10,7 @@
   '';
 
   system = {
-    # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-    activationScripts.postUserActivation.text = ''
-      # activateSettings -u will reload the settings from the database and apply them to the current session,
-      # so we do not need to logout and login again to make the changes take effect.
-      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-    '';
+    primaryUser = vars.username;
 
     defaults = {
       # show 24 hour clock
