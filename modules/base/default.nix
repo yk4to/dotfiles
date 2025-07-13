8c1ca33fad772c9f76dce8d8dd5d3409cf9cc3f2
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-07-13 22:05:12 +0900

    chore(nix): add hyprland cachix config

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -18,11 +18,13 @@
     substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org/"
       "https://ghostty.cachix.org/"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
     ];
   };
