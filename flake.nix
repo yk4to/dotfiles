caeb3fec8a080719902c769404c6db7c25dcc427
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-09-07 15:37:14 +0900

    chore(nix): remove deploy-rs

--- flake.nix
+++ flake.nix
@@ -24,8 +24,6 @@
 
     agenix.url = "github:yaxitech/ragenix";
 
-    deploy-rs.url = "github:serokell/deploy-rs";
-
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -55,30 +53,10 @@
     {
       nixosConfigurations = hosts.nixos;
       darwinConfigurations = hosts.darwin;
-
-      deploy = {
-        user = "root";
-
-        autoRollback = true;
-        magicRollback = true;
-
-        remoteBuild = true;
-
-        nodes.raspi4 = {
-          hostname = "raspi4.local";
-          profiles.system.path = inputs.deploy-rs.lib.aarch64-linux.activate.nixos hosts.nixos.raspi4;
-        };
-      };
-
-      # checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks inputs.self.deploy) inputs.deploy-rs.lib;
     }
     // inputs.flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import inputs.nixpkgs {inherit system;};
     in {
       formatter = pkgs.alejandra;
-
-      devShells.default = pkgs.mkShell {
-        buildInputs = [pkgs.deploy-rs];
-      };
     });
 }
