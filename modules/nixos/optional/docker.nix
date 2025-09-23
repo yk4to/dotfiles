d1052237cbf2dcc9e760f4dfb451be1c5720610f
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-09-23 14:25:09 +0900

    fix(docker): small fixes

--- modules/nixos/optional/docker.nix
+++ modules/nixos/optional/docker.nix
@@ -8,14 +8,12 @@
 with lib; let
   cfg = config.optionalModules.nixos.docker;
 in {
-  imports = mylib.scanPaths ./.;
-
   options.optionalModules.nixos.docker = {
     enable = mkEnableOption "Docker";
   };
 
   config = mkIf cfg.enable {
     virtualisation.docker.enable = true;
-    users.users.${vars.userName}.extraGroups = ["docker"];
+    users.users.${vars.username}.extraGroups = ["docker"];
   };
 }
