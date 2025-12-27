146439cb5761fd8ba2bfd8aaaeab67be4c3cf9ac
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-12-27 15:43:34 +0900

    refactor(darwin): remove custom icons for unused apps

--- modules/darwin/icons.nix
+++ modules/darwin/icons.nix
@@ -15,48 +15,12 @@ in {
     enable = true;
     icons = [
       {
-        path = "/Applications/AltTab.app";
-        icon = "${iconsPath}/alttab.icns";
-      }
-      {
-        path = "/Applications/KeyboardCleanTool.app/";
-        icon = "${iconsPath}/keyboardcleantool.icns";
-      }
-      {
         path = "/Applications/Blackmagic RAW/Blackmagic RAW Player.app/";
         icon = "${iconsPath}/rawplayer.icns";
       }
       {
         path = "/Applications/Blackmagic RAW/Blackmagic RAW Speed Test.app/";
         icon = "${iconsPath}/rawspeedtest.icns";
-      }
-      {
-        path = "/Applications/Google Docs.app/";
-        icon = "${iconsPath}/googledocs.icns";
-      }
-      {
-        path = "/Applications/Google Sheets.app/";
-        icon = "${iconsPath}/googlesheets.icns";
-      }
-      {
-        path = "/Applications/Google Slides.app/";
-        icon = "${iconsPath}/googleslides.icns";
-      }
-      {
-        path = "/Applications/Box.app/";
-        icon = "${iconsPath}/box.icns";
-      }
-      {
-        path = "/Applications/F5 VPN.app/";
-        icon = "${iconsPath}/f5vpn.icns";
-      }
-      {
-        path = "/Applications/Raspberry Pi Imager.app/";
-        icon = "${iconsPath}/raspi.icns";
-      }
-      {
-        path = "/Applications/MATLAB_R2024a.app/";
-        icon = "${iconsPath}/matlab.icns";
       }
       {
         path = "/Applications/KiCad/KiCad.app/";
@@ -69,10 +33,6 @@ in {
       {
         path = "/Applications/FlashPrint 5.app/";
         icon = "${iconsPath}/flashprint.icns";
-      }
-      {
-        path = "/Applications/LINE.app/";
-        icon = "${iconsPath}/line.icns";
       }
       {
         path = "/Applications/Notion.app/";
