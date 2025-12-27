5bca409a49835e5b38e3a5195e04a27f551d0eaf
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-12-27 11:45:29 +0900

    Revert "feat(nix): enable cachix"
    This reverts commit 3582c1629183da143bd6c706ec42253487c6973c.

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -18,12 +18,10 @@
     substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
-      "https://yk4to.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "yk4to.cachix.org-1:KrtNAIH+Uo7aMq5gnohYVCum4Yd4AcNLpktnOCf+Rn4="
     ];
   };
 
