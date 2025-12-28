b83dbcf1a5776291a928e20cb5c478c4243a21d7
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-12-28 11:15:34 +0900

    chore(deps): update lanzaboote and flake.lock

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     agenix.url = "github:yaxitech/ragenix";
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
+      url = "github:nix-community/lanzaboote/v1.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
