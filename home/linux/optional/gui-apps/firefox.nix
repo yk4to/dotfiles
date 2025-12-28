bcbacef44f6c28280ba3852858a44031edd11f6e
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-12-28 11:22:50 +0900

    fix(firefox): enable force option

--- home/linux/optional/gui-apps/firefox.nix
+++ home/linux/optional/gui-apps/firefox.nix
@@ -25,28 +25,31 @@
           wappalyzer
         ];
 
-        bookmarks = [
-          {
-            name = "YouTube";
-            url = "https://www.youtube.com/";
-          }
-          {
-            name = "GitHub";
-            url = "https://github.com/";
-          }
-          {
-            name = "NixOS Search - Packages";
-            url = "https://search.nixos.org/packages?channel=unstable";
-          }
-          {
-            name = "Home Manager - Option Search";
-            url = "https://home-manager-options.extranix.com/";
-          }
-          {
-            name = "Darwin Configuration Options";
-            url = "https://daiderd.com/nix-darwin/manual/index.html";
-          }
-        ];
+        bookmarks = {
+          force = true;
+          settings = [
+            {
+              name = "YouTube";
+              url = "https://www.youtube.com/";
+            }
+            {
+              name = "GitHub";
+              url = "https://github.com/";
+            }
+            {
+              name = "NixOS Search - Packages";
+              url = "https://search.nixos.org/packages?channel=unstable";
+            }
+            {
+              name = "Home Manager - Option Search";
+              url = "https://home-manager-options.extranix.com/";
+            }
+            {
+              name = "Darwin Configuration Options";
+              url = "https://daiderd.com/nix-darwin/manual/index.html";
+            }
+          ];
+        };
       };
     };
   };
