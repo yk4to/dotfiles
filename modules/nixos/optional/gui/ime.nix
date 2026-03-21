9dc86600441a4ca229021abf5d0b6e777f1cb0de
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-03-21 21:11:37 +0900

    fix(ime): enable mozc by default

--- modules/nixos/optional/gui/ime.nix
+++ modules/nixos/optional/gui/ime.nix
@@ -8,7 +8,29 @@
     i18n.inputMethod = {
       enable = true;
       type = "fcitx5";
-      fcitx5.addons = [pkgs.fcitx5-mozc];
+      fcitx5 = {
+        addons = [pkgs.fcitx5-mozc];
+
+        # enable mozc
+        settings.inputMethod = {
+          GroupOrder = {
+            "0" = "Default";
+          };
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "mozc";
+          };
+          "Groups/0/Items/0" = {
+            Name = "keyboard-us";
+            Layout = "";
+          };
+          "Groups/0/Items/1" = {
+            Name = "mozc";
+            Layout = "";
+          };
+        };
+      };
     };
   };
 }
