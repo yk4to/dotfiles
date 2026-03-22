7b72d0e3a4b297d511a3a1e5f101566045c23e94
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-03-22 12:45:20 +0900

    feat(niri): add keybinds

---
+++ home/linux/optional/niri/binds.nix
@@ -0,0 +1,73 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.optionalModules.linux.niri;
+
+  noctalia = cmd:
+    [
+      "noctalia-shell"
+      "ipc"
+      "call"
+    ]
+    ++ (pkgs.lib.splitString " " cmd);
+in {
+  config = mkIf cfg.enable {
+    programs.niri.settings = {
+      binds = {
+        "Mod+Shift+Slash".action.show-hotkey-overlay = [];
+
+        "Mod+Q" = {
+          repeat = false;
+          action.close-window = [];
+        };
+
+        "Mod+Left".action.focus-column-left = [];
+        "Mod+Down".action.focus-window-down = [];
+        "Mod+Up".action.focus-window-up = [];
+        "Mod+Right".action.focus-column-right = [];
+
+        "Mod+Ctrl+Left".action.move-column-left = [];
+        "Mod+Ctrl+Down".action.move-window-down = [];
+        "Mod+Ctrl+Up".action.move-window-up = [];
+        "Mod+Ctrl+Right".action.move-column-right = [];
+
+        "Mod+1".action.focus-workspace = 1;
+        "Mod+2".action.focus-workspace = 2;
+        "Mod+3".action.focus-workspace = 3;
+        "Mod+4".action.focus-workspace = 4;
+        "Mod+5".action.focus-workspace = 5;
+        "Mod+6".action.focus-workspace = 6;
+        "Mod+7".action.focus-workspace = 7;
+        "Mod+8".action.focus-workspace = 8;
+        "Mod+9".action.focus-workspace = 9;
+        "Mod+Shift+1".action.move-column-to-workspace = 1;
+        "Mod+Shift+2".action.move-column-to-workspace = 2;
+        "Mod+Shift+3".action.move-column-to-workspace = 3;
+        "Mod+Shift+4".action.move-column-to-workspace = 4;
+        "Mod+Shift+5".action.move-column-to-workspace = 5;
+        "Mod+Shift+6".action.move-column-to-workspace = 6;
+        "Mod+Shift+7".action.move-column-to-workspace = 7;
+        "Mod+Shift+8".action.move-column-to-workspace = 8;
+        "Mod+Shift+9".action.move-column-to-workspace = 9;
+
+        "Mod+Minus".action.set-column-width = "-10%";
+        "Mod+Equal".action.set-column-width = "+10%";
+
+        "Mod+Shift+Minus".action.set-window-height = "-10%";
+        "Mod+Shift+Equal".action.set-window-height = "+10%";
+
+        "Mod+V".action.toggle-window-floating = [];
+        "Mod+Shift+V".action.switch-focus-between-floating-and-tiling = [];
+
+        "Mod+Space".action.spawn = noctalia "launcher toggle";
+        "XF86AudioLowerVolume".action.spawn = noctalia "volume decrease";
+        "XF86AudioRaiseVolume".action.spawn = noctalia "volume increase";
+        "XF86AudioMute".action.spawn = noctalia "volume muteOutput";
+      };
+    };
+  };
+}
--- home/linux/optional/niri/default.nix
+++ home/linux/optional/niri/default.nix
@@ -2,7 +2,6 @@
   config,
   lib,
   mylib,
-  pkgs,
   ...
 }:
 with lib; let
@@ -15,25 +14,20 @@ in {
   };
 
   config = mkIf cfg.enable {
-    programs.niri.settings = let
-      noctalia = cmd:
-        [
-          "noctalia-shell"
-          "ipc"
-          "call"
-        ]
-        ++ (pkgs.lib.splitString " " cmd);
-    in {
-      binds = {
-        "Mod+Space".action.spawn = noctalia "launcher toggle";
-        "XF86AudioLowerVolume".action.spawn = noctalia "volume decrease";
-        "XF86AudioRaiseVolume".action.spawn = noctalia "volume increase";
-        "XF86AudioMute".action.spawn = noctalia "volume muteOutput";
-      };
-
+    programs.niri.settings ={
       outputs."eDP-1".scale = 1.1;
 
       input.touchpad.natural-scroll = false;
+
+      window-rules = [
+        {
+          matches = [{
+            app-id = "firefox$";
+            title = "^Picture-in-Picture$";
+          }];
+          open-floating = true;
+        }
+      ];
     };
   };
 }
