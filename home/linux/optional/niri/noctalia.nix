a4aa9e18da5ff11db0b58adc3c6f27b76b0bb346
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-03-29 14:28:51 +0900

    feat(noctalia): add config

--- home/linux/optional/niri/noctalia.nix
+++ home/linux/optional/niri/noctalia.nix
@@ -47,12 +47,95 @@
 
         general.avatarImage = "${../../../../icon.jpg}";
 
-        location = {
-          monthBeforeDay = true;
-          name = "Tokyo, Japan";
-        };
+        location.name = "Tokyo, Japan";
 
         appLauncher.terminalCommand = "ghostty --window-decoration=false";
+
+        bar = {
+          widgets = {
+            left = [
+              {
+                id = "Launcher";
+                useDistroLogo = true;
+                enableColorization = true;
+              }
+              {
+                id = "Workspace";
+              }
+              {
+                id = "SystemMonitor";
+                compactMode = true;
+              }
+              {
+                id = "MediaMini";
+              }
+            ];
+            center = [
+              {
+                id = "ActiveWindow";
+              }
+            ];
+            right = [
+              {
+                id = "Tray";
+                drawerEnabled = false;
+              }
+              {
+                id = "NotificationHistory";
+              }
+              {
+                id = "Battery";
+                showPowerProfiles = true;
+              }
+              {
+                id = "Volume";
+              }
+              {
+                id = "Brightness";
+              }
+              {
+                id = "KeepAwake";
+              }
+              {
+                id = "Clock";
+                formatHorizontal = "yyyy/MM/dd (ddd) HH:mm";
+                tooltipFormat = "yyyy/MM/dd (ddd) HH:mm";
+              }
+              {
+                id = "ControlCenter";
+              }
+            ];
+          };
+        };
+
+        controlCenter = {
+          cards = [
+            {
+              id = "profile-card";
+              enabled = true;
+            }
+            {
+              id = "shortcuts-card";
+              enabled = true;
+            }
+            {
+              id = "audio-card";
+              enabled = true;
+            }
+            {
+              id = "brightness-card";
+              enabled = false;
+            }
+            {
+              id = "weather-card";
+              enabled = false;
+            }
+            {
+              id = "media-sysmon-card";
+              enabled = false;
+            }
+          ];
+        };
       };
     };
   };
