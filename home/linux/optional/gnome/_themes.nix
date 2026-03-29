12c99d4d3cf7ddaab19b58762ac1c1c89deef6ae
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-03-29 19:01:06 +0900

    fix(themes): update gtk theme settings to use catppuccin

--- home/linux/optional/gnome/_themes.nix
+++ home/linux/optional/gnome/_themes.nix
@@ -1,19 +1,17 @@
 {
-  pkgs,
   lib,
   config,
   ...
-}: {
+}: let
+  flavor = "mocha";
+  accent = "blue";
+in {
   config = lib.mkIf config.optionalModules.linux.gnome.enable {
-    home.packages = with pkgs; [
-      yaru-theme
-    ];
-
     dconf.settings = {
       "org/gnome/desktop/interface" = {
-        cursor-theme = "Yaru";
-        gtk-theme = "Yaru-blue-dark";
-        icon-theme = "Yaru-blue";
+        cursor-theme = "catppuccin-${flavor}-${accent}-cursors";
+        # gtk-theme = "Yaru-blue-dark";
+        icon-theme = "Papirus-Dark";
         color-scheme = "prefer-dark";
       };
     };
@@ -21,23 +19,23 @@
     gtk = {
       enable = true;
 
-      iconTheme = {
-        package = pkgs.yaru-theme;
-        name = "Yaru-blue";
-      };
+      # iconTheme = {
+      #   package = pkgs.yaru-theme;
+      #   name = "Yaru-blue";
+      # };
 
-      theme = {
-        package = pkgs.yaru-theme;
-        name = "Yaru-blue-dark";
-      };
+      # theme = {
+      #   package = pkgs.yaru-theme;
+      #   name = "Yaru-blue-dark";
+      # };
 
       gtk3.extraConfig = {
-        gtk-icon-theme-name = "Yaru";
-        gtk-theme-name = "Yaru-blue-dark";
+        gtk-icon-theme-name = "Papirus-Dark";
+        # gtk-theme-name = "Yaru-blue-dark";
         gtk-application-prefer-dark-theme = 1;
       };
 
-      gtk4.theme = config.gtk.theme;
+      # gtk4.theme = config.gtk.theme;
     };
   };
 }
