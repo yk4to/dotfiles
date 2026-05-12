de28617813bdd95954c52498f50bbc4b011a7086
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-05-12 16:01:59 +0900

    fix(nix): add missing mkIf

--- modules/nixos/optional/gui/catppuccin.nix
+++ modules/nixos/optional/gui/catppuccin.nix
@@ -1,6 +1,12 @@
 {
-  catppuccin.cursors = {
-    enable = true;
-    accent = "mauve";
+  lib,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.optionalModules.nixos.gui.enable {
+    catppuccin.cursors = {
+      enable = true;
+      accent = "mauve";
+    };
   };
 }
