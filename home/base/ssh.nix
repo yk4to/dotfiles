7e60f844ae84e9f62e0332cc1e4c1513ce188002
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-05-29 17:05:34 +0900

    refactor(ssh): remove unused matchBlocks config

--- home/base/ssh.nix
+++ home/base/ssh.nix
@@ -6,21 +6,9 @@
   programs.ssh = {
     enable = true;
 
+    # Keep Home Manager from reintroducing the legacy default Host * block.
     # This option will become deprecated in the future.
     enableDefaultConfig = false;
-
-    matchBlocks."*" = {
-      forwardAgent = false;
-      addKeysToAgent = "no";
-      compression = false;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = false;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-      controlMaster = "no";
-      controlPath = "~/.ssh/master-%r@%n:%p";
-      controlPersist = "no";
-    };
 
     # Include Orbstack config on macOS
     includes = lib.optional isDarwin "~/.orbstack/ssh/config";
