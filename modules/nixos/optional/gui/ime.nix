a65a48fee9eadf227d9e44217663dd0c239de58f
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-05-31 13:23:02 +0900

    feat(hazkey): add nix-hazkey module and configure ime

--- modules/nixos/optional/gui/ime.nix
+++ modules/nixos/optional/gui/ime.nix
@@ -1,18 +1,21 @@
 {
-  pkgs,
+  inputs,
   lib,
   config,
   ...
 }: {
+  imports = [inputs.nix-hazkey.nixosModules.hazkey];
+
   config = lib.mkIf config.optionalModules.nixos.gui.enable {
+    services.hazkey.enable = true;
+
     i18n.inputMethod = {
       enable = true;
       type = "fcitx5";
       fcitx5 = {
         waylandFrontend = true;
-        addons = [pkgs.fcitx5-mozc];
 
-        # enable mozc
+        # Preconfigure Hazkey in the Fcitx profile.
         settings.inputMethod = {
           GroupOrder = {
             "0" = "Default";
@@ -20,14 +23,14 @@
           "Groups/0" = {
             Name = "Default";
             "Default Layout" = "us";
-            DefaultIM = "mozc";
+            DefaultIM = "hazkey";
           };
           "Groups/0/Items/0" = {
             Name = "keyboard-us";
             Layout = "";
           };
           "Groups/0/Items/1" = {
-            Name = "mozc";
+            Name = "hazkey";
             Layout = "";
           };
         };
