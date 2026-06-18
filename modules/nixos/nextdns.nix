84896b6dcc49544eda5e010a395f4f3e60c92632
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-06-18 12:40:34 +0900

    fix(nextdns): disable on wsl

--- modules/nixos/nextdns.nix
+++ modules/nixos/nextdns.nix
@@ -1,22 +1,27 @@
 {
   inputs,
   config,
+  lib,
   ...
-}: {
-  services.nextdns = {
-    enable = true;
-    arguments = ["-config-file" "${config.age.secrets.nextdns.path}"];
-  };
+}: let
+  isWsl = config.wsl.enable or false;
+in {
+  config = lib.mkIf (!isWsl) {
+    services.nextdns = {
+      enable = true;
+      arguments = ["-config-file" "${config.age.secrets.nextdns.path}"];
+    };
 
-  networking.nameservers = [
-    "127.0.0.1"
-    "::1"
-  ];
+    networking.nameservers = [
+      "127.0.0.1"
+      "::1"
+    ];
 
-  age.secrets = {
-    nextdns = {
-      file = "${inputs.secrets}/nextdns.age";
-      mode = "600";
+    age.secrets = {
+      nextdns = {
+        file = "${inputs.secrets}/nextdns.age";
+        mode = "600";
+      };
     };
   };
 }
