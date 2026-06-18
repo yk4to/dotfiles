b6f8ac9df7ac15f6e7d12d17056c64ab33c1caf9
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-06-18 12:11:39 +0900

    feat: add wsl2 machine

---
+++ hosts/lucy-wsl/home-manager.nix
@@ -0,0 +1,14 @@
+{
+  optionalModules = {
+    base = {
+      # ghostty.enable = true;
+      # latex.enable = true;
+      # platformio.enable = true;
+      # vscode.enable = true;
+    };
+    linux = {
+      # gnome.enable = true;
+      # gui-apps.enable = true;
+    };
+  };
+}
