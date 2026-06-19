bf06f56d19b8b1d2899ae36ac9bd3a3634ce9fb9
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-06-19 19:04:22 +0900

    feat(user): set user password from hash file

--- modules/base/users.nix
+++ modules/base/users.nix
@@ -1,11 +1,20 @@
 {
+  config,
+  inputs,
+  lib,
   pkgs,
   vars,
   isDarwin,
   ...
 }: {
+  age.secrets = lib.mkIf (!isDarwin) {
+    user-password = {
+      file = "${inputs.secrets}/password.age";
+      mode = "600";
+    };
+  };
+
   # Define a user account.
-  # Don't forget to set a password with ‘passwd’ on NixOS.
   users.users.${vars.username} =
     {
       description = vars.userfullname;
@@ -20,6 +29,8 @@
         home = "/home/${vars.username}";
         isNormalUser = true;
         extraGroups = ["wheel"];
+        # password.age must contain a single mkpasswd-compatible hash line.
+        hashedPasswordFile = config.age.secrets.user-password.path;
       }
     );
 }
