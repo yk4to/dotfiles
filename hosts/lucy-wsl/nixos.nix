535e401239fa6080aabc80baac78a686edadaeb4
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-06-20 11:24:09 +0900

    fix(wsl): patch vscode remote

--- hosts/lucy-wsl/nixos.nix
+++ hosts/lucy-wsl/nixos.nix
@@ -1,8 +1,15 @@
-{...}: {
+{pkgs, ...}: {
   optionalModules.nixos = {
     docker.enable = true;
     wsl.enable = true;
     tailscale.enable = true;
     vscode-server.enable = true;
   };
+
+  # Required by NixOS-WSL's VS Code Remote setup guide.
+  # ref: https://nix-community.github.io/NixOS-WSL/how-to/vscode.html
+  environment.systemPackages = with pkgs; [
+    wget
+  ];
+  programs.nix-ld.enable = true;
 }
