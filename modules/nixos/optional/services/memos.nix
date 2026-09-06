868f53d4e6d7e6422a65a5dbca7a1261a9a8936e
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-09-06 18:01:51 +0900

    fix(memos): add env

--- modules/nixos/optional/services/memos.nix
+++ modules/nixos/optional/services/memos.nix
@@ -9,11 +9,14 @@
     ];
 
     virtualisation.oci-containers.containers.memos = {
-      image = "neosmemo/memos:0.30.0";
+      image = "docker.io/neosmemo/memos:0.30.0";
       volumes = [
         "/srv/data/memos:/var/opt/memos"
       ];
       ports = ["5230:5230"];
+      environment = {
+        "MEMOS_INSTANCE_URL" = "https://memos.fus1on.dev";
+      };
     };
 
     networking.firewall.allowedTCPPorts = [5230];
